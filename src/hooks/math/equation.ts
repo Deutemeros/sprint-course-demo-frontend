e551219157653406342d4c24e4486140ff101bf3
Morgane Officer <amadis.lemore@gmail.com>
2025-02-21 15:56:27 +0100

    _

--- src/hooks/math/equation.ts
+++ src/hooks/math/equation.ts
@@ -3,13 +3,13 @@ import { get, post } from "../core"
 
 
 export const getNewEquation = async (): Promise<Equation> => {
-    const body = await get<Equation>("api/math");
+    const body = await get<Equation>("api/fastmath");
     return body;
 }
 
 
 export const postEquationResult = async (equation: Equation, results: number): Promise<boolean> => {
-    const body = await post<boolean>("api/math", {
+    const body = await post<boolean>("api/fastmath", {
         eq: equation,
         results: results
     } as EquationResults, {
