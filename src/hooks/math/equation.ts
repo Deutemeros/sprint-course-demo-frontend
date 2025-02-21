4f251f5989b0ebf1931bd675a76c3fe2be895213
Morgane Officer <unknown.travelers0@gmail.com>
2025-02-21 15:12:59 +0100

    change fastmath api resource path

--- src/hooks/math/equation.ts
+++ src/hooks/math/equation.ts
@@ -3,13 +3,13 @@ import { get, post } from "../core"
 
 
 export const getNewEquation = async (): Promise<Equation> => {
-    const body = await get<Equation>("api");
+    const body = await get<Equation>("api/math");
     return body;
 }
 
 
 export const postEquationResult = async (equation: Equation, results: number): Promise<boolean> => {
-    const body = await post<boolean>("api", {
+    const body = await post<boolean>("api/math", {
         eq: equation,
         results: results
     } as EquationResults, {
